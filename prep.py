9b05ec8393705b0d72f7983d2532f1435ae68f42
Aaron Virshup <avirshup@gmail.com>
2017-09-12 06:58:05 -0700

    Faster charge method

--- prep.py
+++ prep.py
@@ -7,6 +7,6 @@ def setup_forcefield(mol):
     """
     Create force field parameters for the chosen ligand
     """
-    ff = mdt.create_ff_parameters(mol, charges='am1-bcc', baseff='gaff2')
+    ff = mdt.create_ff_parameters(mol, charges='gasteiger', baseff='gaff2')
     ff.assign(mol)
     return ff
